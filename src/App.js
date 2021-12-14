687b877e75c58e2a702be24ed36dce194992af56
bilal <bilal.gkdg@gmail.com>
2021-12-14 10:43:03 +0300

    Latest fixes & last commit.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Header from './components/Header';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Punklist from './components/Punklist';
+import Main from './components/Main';
 
 function App() {
   const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
 
   useEffect(() => {
     const getMyNfts = async () => {
@@ -20,7 +22,15 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <Punklist punkListData={punkListData} />
+      {punkListData.length > 0 && (
+        <>
+          <Main punkListData={punkListData} selectedPunk={selectedPunk} />
+          <Punklist
+            punkListData={punkListData}
+            setSelectedPunk={setSelectedPunk}
+          />
+        </>
+      )}
     </div>
   );
 }
--- src/components/Punklist.js
+++ src/components/Punklist.js
@@ -2,11 +2,11 @@ import React from 'react';
 import CollectionCard from '../components/CollectionCard';
 import './Punklist.css';
 
-const Punklist = ({ punkListData }) => {
+const Punklist = ({ punkListData, setSelectedPunk }) => {
   return (
     <div className="punkList">
       {punkListData.map(punk => (
-        <div>
+        <div key={punk.token_id} onClick={() => setSelectedPunk(punk.token_id)}>
           <CollectionCard
             key={punk.token_id}
             id={punk.token_id}
